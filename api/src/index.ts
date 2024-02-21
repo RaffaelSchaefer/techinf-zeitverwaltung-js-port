acc99862f36e9689807bf4b28667434e6588a318
Raffael Elias Schäfer <s20a3c@umwelt-campus.de>
2024-02-21 10:31:59 +0100

    moved files

--- api/src/index.ts
+++ api/src/index.ts
@@ -10,7 +10,7 @@ const port = process.env.NODE_PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api", catalogRouter);
+app.use("/", catalogRouter);
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
